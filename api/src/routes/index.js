aff62b59a03d6da69d3030f9688658292d9526d7
eseval <90922661+eseval@users.noreply.github.com>
2022-03-28 14:26:39 -0500

    commit

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,12 +1,25 @@
 const { Router } = require('express');
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
+const axios = require('axios');
 
 
 const router = Router();
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
-
+const getApiInfo = async () => {
+    const apiUrl = await axios.get('https://api.spoonacular.com/recipes/complexSearch');
+    const apiInfo = await apiUrl.data.map(e => {
+        return {
+            id: e.id,
+            title: e.title,
+            summary: e.summary,
+            score: e.score,
+            healthScore: e.healthScore,
+            steps: e.steps
+        }
+    })
+}
 
 module.exports = router;
